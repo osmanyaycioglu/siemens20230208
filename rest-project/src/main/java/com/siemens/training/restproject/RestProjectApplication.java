42263d76342a43561950370df57c18105230a5f2
osman <osman@osy.com>
2023-02-23 11:20:01 +0300

    day 14 3

---
+++ rest-project/src/main/java/com/siemens/training/restproject/RestProjectApplication.java
@@ -0,0 +1,14 @@
+package com.siemens.training.restproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestProjectApplication.class,
+                              args);
+    }
+
+}
---
+++ rest-project/src/test/java/com/siemens/training/restproject/RestProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.siemens.training.restproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

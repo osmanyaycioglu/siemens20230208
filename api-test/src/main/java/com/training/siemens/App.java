97918251aeffa48b519f5fa11f6d91f489d00241
osman <osman@osy.com>
2023-02-24 12:05:08 +0300

    day 15 1

--- api-test/src/main/java/com/training/siemens/App.java
+++ api-test/src/main/java/com/training/siemens/App.java
@@ -15,28 +15,33 @@ public class App {
         HikariDbPool hikariDbPool = new HikariDbPool();
         try (Connection connection = hikariDbPool.getConnection();
              PreparedStatement ps = connection.prepareStatement("INSERT INTO person (person_id, person_name, person_surname) VALUES (?,?,?)");
-             PreparedStatement pss = connection.prepareStatement("select * from person");
-             ResultSet resultSet = pss.executeQuery();
              ) {
 
             ps.setLong(1,
-                       100);
+                       101);
             ps.setString(2,
-                         "osman");
+                         "deneme");
             ps.setString(3,
                          "yaycıoğlu");
             ps.execute();
-            ps.flu
+
+        } catch (SQLException eParam) {
+            eParam.printStackTrace();
+        }
+
+        try (Connection connection = hikariDbPool.getConnection();
+             PreparedStatement pss = connection.prepareStatement("select * from person");
+             ResultSet resultSet = pss.executeQuery();
+        ) {
             while (resultSet.next()){
                 long personId = resultSet.getLong("person_id");
                 String personName = resultSet.getString("person_name");
                 String personSurname = resultSet.getString("person_surname");
                 System.out.println("pid : " + personId + " pn : " + personName + " ps : " + personSurname);
             }
-
-
         } catch (SQLException eParam) {
             eParam.printStackTrace();
         }
+
     }
 }
